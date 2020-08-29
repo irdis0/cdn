0db902ac0d36e649efd57b877907a7b8488ed0e7
MoGuJun <ye8980@gmail.com>
2020-08-30 03:43:23 +0800

    修改图片外链

--- Sakura/js/sakura-app.js
+++ Sakura/js/sakura-app.js
@@ -2041,7 +2041,7 @@ function loadBotui() {
 		if (mashiro_global.variables.has_bot_ui) {
 			bot_ui_ini()
 		} else {
-			$.getScript('https://cdn.jsdelivr.net/gh/Fog-Forest/cdn@2.1.2/botui/botui.js',
+			$.getScript('https://cdn.jsdelivr.net/gh/Fog-Forest/cdn@2.1.4/botui/botui.js',
 			function() {
 				bot_ui_ini();
 				mashiro_global.variables.has_bot_ui = true
